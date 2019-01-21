c608055b046c0d82389ca3bba5abe8b9d35548c8
Fabio De Santi <fsanti68@hotmail.com>
2019-01-21 13:59:09 -0200

    Added ALL option for rows and/or columns on command line

--- index.js
+++ index.js
@@ -9,21 +9,27 @@ if (argv.length == 0) {
   console.log("  color [row[-row]] [col[-col]] [color name]");
   console.log("\nExamples:");
   console.log("  # node index.js mode snake                     starts snake mode");
-  console.log("  # node index.js color 0 0-18 red               red on all row 0 keys");
-  console.log("  # node index.js color 0,5 0-18 red \\           red on rows 0 and 5");
-  console.log("       color 2-4 0-20 white                      and white on rows 2 to 4");
+  console.log("  # node index.js color 0 ALL red                red on all row 0 keys");
+  console.log("  # node index.js color 0,5 ALL red \\            red on rows 0 and 5");
+  console.log("       color 2-4 ALL white                       and white on rows 2 to 4");
   console.log("  # node index.js color 1,4-5 0-1,18-19 yellow   yellow for the first and");
   console.log("                                                 last 2 keys of rows 1, 4 and 5");
   process.exit(0);
 }
 
-function getrange(s) {
+function getrange(s, max) {
   let r = [];
   let l = s.split(',');
   for (let i = 0; i < l.length; i++) {
     let x = l[i].split('-');
     if (x.length === 1) {
-      r.push(parseInt(x[0]));
+      if (x[0] === 'ALL') {
+        for (let j = 0; j <= max; j++) {
+          r.push(j);
+        }
+      } else {
+        r.push(parseInt(x[0]));
+      }
     } else {
       for (let j = parseInt(x[0]); j <= parseInt(x[1]); j++) {
         r.push(j);
@@ -43,8 +49,8 @@ k.initialize(() => {
 
     } else if (argv[c] === 'color') {
 
-      let rows = getrange(argv[++c]);
-      let cols = getrange(argv[++c]);
+      let rows = getrange(argv[++c], 5);
+      let cols = getrange(argv[++c], 21);
       let color = argv[++c];
       //console.debug("set '" + color + "' on rows " + rows + " and columns " + cols);
       for (let row in rows) {
